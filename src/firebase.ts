5a603784734ce405829ae2d6a4e3c6cc054fae34
patohsi <115231305+patohsh@users.noreply.github.com>
2026-04-29 02:45:34 +0900

    복구

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,17 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Analytics 초기화 (브라우저 + 지원 체크)
-let analytics: ReturnType<typeof getAnalytics> | undefined;
-
-if (typeof window !== "undefined") {
-    isSupported().then((yes) => {
-        if (yes) {
-            analytics = getAnalytics(app);
-        }
-    });
-}
-
-
 export const db = getFirestore(app);
